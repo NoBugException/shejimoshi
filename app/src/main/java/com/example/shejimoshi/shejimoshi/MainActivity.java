76e138eedb92d3b11bfd8fed0edf8450afa2e53b
zhouyunchong <804431438@qq.com>
2018-03-26 00:58:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/shejimoshi/shejimoshi/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.shejimoshi.shejimoshi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 设计模式
+ * 创建型设计模式5种：单例模式，抽象工厂模式，工厂模式，原型模式，建造者模式（口诀：单原建造者，东西二厂）
+ * 结构型模式：适配器模式，桥接模式，装饰模式，组合模式，外观模式，享元模式，代理模式。（口诀：一器一桥一元一代理；装饰组合外观）
+ * 行为型模式11种：观察者模式，中介者模式，访问者模式，解释器模式，迭代器模式，备忘录模式，责任链模式，状态模式，策略模式，命令模式，模板模式。 （口诀：三者两器、一录一链一模板，状态策略命令）
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
